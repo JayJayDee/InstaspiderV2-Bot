1653ed8c04df857daa7b308693080f2080c05dfa
JayJayDee <jindongp@gmail.com>
2019-04-25 19:01:12 +0900

    added configuration read-routine.

--- README.md
+++ README.md
@@ -4,5 +4,19 @@ instaspider-v2 bot for instaspiderV2 services. using Google Puppeteer api.
 ## Getting started
 ```bash
 $ npm install
-$ npm run dev
+$ CONFIG_PATH=configpath npm run start
+```
+
+## Configuration file format
+instaspider-v2-bot uses amqp protocol as for a messsage exchange.
+so you must supply the below configuration file before run.
+```json
+{
+   "amqp":{
+      "hostname":"HOST_NAME",
+      "port":PORT,
+      "username":"USERNAME",
+      "password":"PASSWORD"
+   }
+}
 ```
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,21 +1,25 @@
+import * as jsonfile from 'jsonfile';
 import { RootConfig, AmqpConfig } from './types';
 
 let root: RootConfig = null;
 const rootConfig = async (): Promise<RootConfig> => {
   if (root === null) {
-    root = {
-      amqp: {
-        hostname: 'chatpot.chat',
-        port: 56720,
-        username: 'jaydee',
-        password: 'hu77lzg5'
-      }
-    };
+    root = await readConfig();
   }
   return root;
 };
 
 export const amqp = async (): Promise<AmqpConfig> =>
   (await rootConfig()).amqp;
+
+const readConfig = (): Promise<RootConfig> =>
+  new Promise((resolve, reject) => {
+    const path = process.env.CONFIG_PATH;
+    if (!path) return reject(new Error('CONFIG_PATH environment variable not supplied'));
+    jsonfile.readFile(path, (err, data) => {
+      if (err) return reject(new Error(`config file not found or invalid: ${path}`));
+      resolve(data as RootConfig);
+    });
+  });
 
 export { AmqpConfig } from './types';
