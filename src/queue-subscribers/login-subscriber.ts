56d2e5b3a585f3bb2cf7d2543629fe4bd866fd02
JayJayDee <jindongp@gmail.com>
2019-04-25 11:48:50 +0900

    added login-subscriber code.

--- src/queue-subscribers/login-subscriber.ts
+++ src/queue-subscribers/login-subscriber.ts
@@ -1,5 +1,6 @@
-import { Connection } from 'amqplib';
+import { Connection, Channel } from 'amqplib';
 import { Page } from 'puppeteer';
+import loginCommand from '../commands/login';
 
 const queueName = 'command';
 
@@ -9,7 +10,32 @@ export default (connection: Connection, page: Page) =>
     await channel.assertQueue(queueName);
     console.log(`* queue consume started: ${queueName}`);
     await channel.consume('command', async (msg) => {
-      console.log(`* message arrival from queue: ${queueName}`);
-      console.log(msg);
+      const login = handleLogin(page, channel);
+
+      let parsed: any = null;
+      try {
+        parsed = JSON.parse(msg.content.toString());
+        console.log(`* message arrival from queue: ${queueName}`);
+        console.log(msg);
+        if (parsed.type === 'login') login(parsed);
+      } catch (err) {
+        console.log('* invalid payload. ignored.');
+      }
     }, { noAck: true });
+  };
+
+const sendResponse = async (channel: Channel, payload: any) => {
+  await channel.assertQueue('bot_response');
+  await channel.sendToQueue('bot_response', Buffer.from(JSON.stringify(payload)));
+};
+
+const handleLogin = (page: Page, channel: Channel) =>
+  async (payload: any) => {
+    await loginCommand(page, {
+      id: payload.email,
+      password: payload.password
+    });
+    await sendResponse(channel, {
+      type: 'login_ok'
+    });
   };
